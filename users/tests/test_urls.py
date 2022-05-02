fb7c0556a5dd1864de06f66bb26aa7d394a5ba81
jakesaunders <jakelsaunders94@gmail.com>
2022-05-02 13:08:36 +0100

    Added tests for urls and first view.

---
+++ users/tests/test_urls.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Tests for urls.py."""
+
+
+# 3rd-party
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+# Project
+from users import views
+
+
+class TestURLs(SimpleTestCase):
+    """Test URLS for users app."""
+
+    def test_sign_up_url(self):
+        """Test url."""
+        assert reverse(views.sign_up) == "/users/sign-up"
+
+    def test_log_in_url(self):
+        """Test url."""
+        assert reverse(views.log_in) == "/users/log-in"
+
+    def test_log_out_url(self):
+        """Test url."""
+        assert reverse(views.log_out) == "/users/log-out"
+
+    def test_change_password_url(self):
+        """Test url."""
+        assert reverse(views.change_password) == "/users/change-password"
+
+    def test_password_reset_url(self):
+        """Test url."""
+        assert reverse(views.password_reset) == "/users/reset-password"
+
+    def test_password_reset_confirm_url(self):
+        """Test url."""
+        assert reverse("reset-password-confirm", args=[1, 1]) == "/users/reset-password-confirm/1/1"
---
+++ users/tests/test_views.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""Test for users views."""
+# 3rd-party
+from django.contrib.auth import login
+from django.contrib.auth.hashers import make_password
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.http import HttpResponseRedirect
+from django.test import RequestFactory
+from django.test import TestCase
+from django.urls import reverse
+
+# Project
+from users import views
+from users.forms import CustomUserLoginForm
+from users.tests.factories import CustomUserFactory
+
+
+class TestLogIn(TestCase):
+    """Tests for the login view."""
+
+    def setUp(self) -> None:  # noqa: D102
+        self.url = reverse(views.log_in)
+        self.password = "mypassword"
+        self.user = CustomUserFactory(password=make_password(self.password))
+
+    def test_view_renders_correct_template(self):
+        """View should render the correct template."""
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, "log_in.html")
+
+    def test_view_redirects_if_user_is_logged_in(self):
+        """View should redirect if the user is authed."""
+        self.client.login(email=self.user.email, password=self.password)
+        response = self.client.get(self.url)
+        assert response.url == reverse("index")
+
+    def test_view_renders_correct_form(self):
+        """View should render the corect form."""
+        response = self.client.get(reverse("log-in"))
+        assert isinstance(response.context["form"], CustomUserLoginForm)
+
+    def test_unsuccessful_login_renders_errors(self):
+        """Unsuccessful login should render the applicable errors in the template."""
+        response = self.client.post(
+            self.url, data={"email": "barry@white.com", "password": "not_a_password"}
+        )
+        for error in response.context["form"].errors.values():
+            for error_text in error:
+                assert error_text in str(response.content)
+
+    def test_successful_login_auths_user_and_redirects(self):
+        """A successful login should auth the user and redirect."""
+        response = self.client.post(
+            self.url, data={"email": self.user.email, "password": self.password}
+        )
+        assert response.wsgi_request.user.is_authenticated
+        assert response.url == reverse("index")
