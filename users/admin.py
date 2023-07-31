586d8f17391370bbb372f4fd1356e29476898758
jakesaunders <jake.saunders@prolific.co>
2023-07-31 21:42:39 +0100

    Comma

--- users/admin.py
+++ users/admin.py
@@ -25,7 +25,7 @@ class UserAdmin(DjangoUserAdmin):
                     "is_superuser",
                     "groups",
                     "user_permissions",
-                )
+                ),
             },
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
